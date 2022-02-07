8a243e611bc3cc7afd1f4dd0babe8210c9559046
krishna <krishna@krishnas-mini>
2022-02-07 16:33:47 +0530

    asdf

--- src/test/java/p1/c1.java
+++ src/test/java/p1/c1.java
@@ -5,6 +5,7 @@ import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.Test;
 
 
@@ -16,7 +17,7 @@ public class c1 {
 		
 		TestData testdata = new TestData();
 		System.out.println(testdata.properties.getProperty("deployment"));
-		FileWriter file = new FileWriter("temp/asdf.json");
+		FileWriter file = new FileWriter("temp/"+RandomStringUtils.randomAlphanumeric(10)+".json");
 		file.write("asdfasdfasdfasdfasdfasdfasdfasdf asdfasdfasdfasdfasdfasdfasdf asdfasdfasdfasdfasdfasdf");
 		file.flush();
 		file.close();
